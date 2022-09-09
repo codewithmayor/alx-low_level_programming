9764b79e107fae1041164317c19a58842a425ce7
Mayowa Olawumi <mayor.michael19@gmail.com>
2022-09-09 11:52:58 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - output 
+*
+*Return: 0.
+*/
+int main(void)
+{
+	char hex;
+
+	for (hex = '0'; hex <= '9'; hex++)
+		putchar(hex);
+
+	for (hex = 'a'; hex <= 'f'; hex++)
+		putchar(hex);
+
+	putchar('\n');
+
+	return (0);
+}
